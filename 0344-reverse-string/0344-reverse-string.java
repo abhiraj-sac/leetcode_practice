f90298dedec7a3cac0844691bfac230cd93af32b
abhiraj sachan <99985421+abhiraj-sac@users.noreply.github.com>
2022-10-25 22:32:22 +0530

    Time: 1 ms (99.91%), Space: 57.6 MB (28.21%) - LeetHub

---
+++ 0344-reverse-string/0344-reverse-string.java
@@ -0,0 +1,16 @@
+class Solution {
+    public void reverseString(char[] arr){
+        int start =0;
+        int end= arr.length-1;
+        while(start < end){
+            char temp = arr[start];
+            arr[start] = arr[end];
+            arr[end] = temp;
+            start++;
+            end--;
+        }
+        
+           
+
+    }
+}
